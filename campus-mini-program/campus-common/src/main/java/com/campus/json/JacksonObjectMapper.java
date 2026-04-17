4b9b0bc07da91043c631edd8996ed36f1eeaca8d
girala-byte <1274649357@qq.com>
2026-04-17 15:01:31 +0800

    Update JacksonObjectMapper.java

--- campus-mini-program/campus-common/src/main/java/com/campus/json/JacksonObjectMapper.java
+++ campus-mini-program/campus-common/src/main/java/com/campus/json/JacksonObjectMapper.java
@@ -1,5 +1,5 @@
 package com.campus.json;
-//abcd
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
